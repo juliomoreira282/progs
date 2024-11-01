d3d09cceac74441fad6cc0ad315ef323cab3a95e
Julio Moreira <jujumoreira.cesinha@gmail.com>
2024-11-01 19:51:17 -0300

    git commit

--- sEngSoftware/factorymethod.py
+++ sEngSoftware/factorymethod.py
@@ -1,4 +1,9 @@
 class Animal():
+    def __init__(self, nome, idade, nomeTutor, porte):
+        self.nome = nome
+        self.idade = idade
+        self.nomeTutor = nomeTutor
+        self.porte = porte
     def comunicacao(self):
         print("Sou um animal, portanto, faço barulho.")
     def comportamento(self):
