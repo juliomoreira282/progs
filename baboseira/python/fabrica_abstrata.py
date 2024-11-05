5070d2d3c7edb58cab11c6b2013dd5b9e8149a4c
Julio Moreira <jujumoreira.cesinha@gmail.com>
2024-11-04 21:05:36 -0300

    git commit

---
+++ baboseira/python/fabrica_abstrata.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def __init__(self, nome, idade, nomeTutor, porte):
+        self.nome = nome
+        self.idade = idade
+        self.nomeTutor = nomeTutor
+        self.porte = porte
+    @abstractmethod
+    def comunicacao(self):
+        pass
+    @abstractmethod
+    def comportamento(self):
+        pass
+
+class Cachorro(Animal):
+    def comunicacao(self):
+        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+    def comportamento(self):
+        print(f"Gosto de latir e de brincar com meu/minha dono(a) {self.nomeTutor}.")
+
+class Gato(Animal):
+    def comunicacao(self):
+        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+    def comportamento(self):
+        print(f"Gosto de dormir e de ignorar meu/minha dono(a) {self.nomeTutor}.")
+
+class Passaro(Animal):
+    def comunicacao(self):
+        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+    def comportamento(self):
+        print(f"Gosto muito de cantar para meu/minha dono(a) {self.nomeTutor}.")
+
+class AnimalFactory(ABC):
+    def criarAnimal(self, nome, idade, nomeTutor, porte) -> Animal:
+        pass
+
+class FabricaCachorro(AnimalFactory):
+    def criarAnimal(self, nome, idade, nomeTutor, porte):
+        return Cachorro(nome, idade, nomeTutor, porte)
+
+class FabricaGato(AnimalFactory):
+    def criarAnimal(self, nome, idade, nomeTutor, porte):
+        return Gato(nome, idade, nomeTutor, porte)
+
+class FabricaPassaro(AnimalFactory):
+    def criarAnimal(self, nome, idade, nomeTutor, porte):
+        return Passaro(nome, idade, nomeTutor, porte)
+    
+def criarExibirAnimal(factory: AnimalFactory, nome, idade, nomeTutor, porte):
+    animal = factory.criarAnimal(nome, idade, nomeTutor, porte)
+    animal.comunicacao()
+    animal.comportamento()
+
+criarExibirAnimal(FabricaCachorro(), "Bebeto", 8, "Marcelo", "Pequeno")
+criarExibirAnimal(FabricaGato(), "Bolinha", 7, "Laís", "Pequeno")
+criarExibirAnimal(FabricaPassaro(), "Juninho", 10, "Denise", "Médio")
---
+++ baboseira/python/metodo_fabrica.py
@@ -0,0 +1,53 @@
+class Animal():
+    def __init__(self, nome, idade, nomeTutor, porte):
+        self.nome = nome.capitalize()
+        self.idade = idade
+        self.nomeTutor = nomeTutor.capitalize()
+        self.porte = porte.capitalize()
+    def comunicacao(self):
+        print("Sou um animal. Portanto, faço barulho.")
+    def comportamento(self):
+        print("Essa é minha forma de agir.")
+
+class Cachorro(Animal):
+    def comunicacao(self):
+        print(f"Meu dono é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+    def comportamento(self):
+        print(f"Gosto de latir e de brincar com meu/minha dono(a) {self.nomeTutor}.")
+
+class Gato(Animal):
+    def comunicacao(self):
+        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+    def comportamento(self):
+        print(f"Gosto de dormir e de ignorar meu/minha {self.nomeTutor}.")
+
+class Passaro(Animal):
+    def comunicacao(self):
+        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+    def comportamento(self):
+        print(f"Gosto muito de cantar para meu/minha dono(a) {self.nomeTutor}.")
+
+class AnimalFactory():
+    @staticmethod
+    def criarAnimal(tipo, nome, idade, nomeTutor, porte):
+        match(tipo.capitalize()):
+            case "Cachorro":
+                return Cachorro(nome, idade, nomeTutor, porte)
+            case "Gato":
+                return Gato(nome, idade, nomeTutor, porte)
+            case "Passaro":
+                return Passaro(nome, idade, nomeTutor, porte)
+            case _:
+                return None
+
+c = AnimalFactory.criarAnimal("Cachorro", "Bebeto", 8, "Marcelo", "Pequeno")
+c.comunicacao()
+c.comportamento()
+
+g = AnimalFactory.criarAnimal("gato", "Bolinha", 7, "Laís", "Pequeno")
+g.comunicacao()
+g.comportamento()
+
+p = AnimalFactory.criarAnimal("passaro", "Juninho", 10, "Denise", "Médio")
+p.comunicacao()
+p.comportamento()
