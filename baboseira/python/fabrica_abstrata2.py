7c063ca371ffc1129ae7479ef6248a48567bb864
Julio Moreira <jujumoreira.cesinha@gmail.com>
2024-11-07 17:44:55 -0300

    git commit

--- baboseira/python/fabrica_abstrata2.py
+++ baboseira/python/fabrica_abstrata2.py
@@ -6,30 +6,32 @@ class Animal(ABC):
         self.idade = idade
         self.nomeTutor = nomeTutor
         self.porte = porte
+    
     @abstractmethod
     def comunicacao(self):
         pass
+    
     @abstractmethod
     def comportamento(self):
         pass
 
 class Cachorro(Animal):
     def comunicacao(self):
-        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+        print(f"Meu nome é {self.nome} e meu dono é {self.nomeTutor}.")
     def comportamento(self):
-        print(f"Gosto de latir e de brincar com meu/minha dono(a) {self.nomeTutor}.")
+        print(f"Gosto de latir e de brincar com meu dono {self.nomeTutor}.")
 
 class Gato(Animal):
     def comunicacao(self):
-        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+        print(f"Meu nome é {self.nome} e meu dono é {self.nomeTutor}.")
     def comportamento(self):
-        print(f"Gosto de dormir e de ignorar meu/minha dono(a) {self.nomeTutor}.")
+        print(f"Gosto de dormir e de deixar meu dono {self.nomeTutor} no vácuo.")
 
 class Passaro(Animal):
     def comunicacao(self):
-        print(f"Meu nome é {self.nome} e meu/minha dono(a) é {self.nomeTutor}.")
+        print(f"Meu nome é {self.nome} e meu dono é {self.nomeTutor}.")
     def comportamento(self):
-        print(f"Gosto muito de cantar para meu/minha dono(a) {self.nomeTutor}.")
+        print(f"Gosto muito de cantar para meu dono {self.nomeTutor}.")
 
 class AnimalFactory(ABC):
     @abstractmethod
@@ -39,20 +41,20 @@ class AnimalFactory(ABC):
 class FabricaCachorro(AnimalFactory):
     def criarAnimal(self, nome, idade, nomeTutor, porte):
         return Cachorro(nome, idade, nomeTutor, porte)
-
+    
 class FabricaGato(AnimalFactory):
     def criarAnimal(self, nome, idade, nomeTutor, porte):
         return Gato(nome, idade, nomeTutor, porte)
-
+    
 class FabricaPassaro(AnimalFactory):
     def criarAnimal(self, nome, idade, nomeTutor, porte):
         return Passaro(nome, idade, nomeTutor, porte)
-    
+
 def criarExibirAnimal(factory: AnimalFactory, nome, idade, nomeTutor, porte):
     animal = factory.criarAnimal(nome, idade, nomeTutor, porte)
     animal.comunicacao()
     animal.comportamento()
 
-criarExibirAnimal(FabricaCachorro(), "Bebeto", 8, "Marcelo", "Pequeno")
-criarExibirAnimal(FabricaGato(), "Bolinha", 7, "Tifa", "Pequeno")
-criarExibirAnimal(FabricaPassaro(), "Juninho", 10, "Denise", "Médio")
+criarExibirAnimal(FabricaCachorro(), "Leo", 8, "Francisco", "Grande")
+criarExibirAnimal(FabricaGato(), "Neve", 5, "William", "Pequeno")
+criarExibirAnimal(FabricaPassaro(), "Marcelo", 13, "Gabriel", "Pequeno")
