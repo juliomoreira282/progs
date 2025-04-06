e69c1c8fddec9ad2de581b7058a4582622359709
Julio Moreira <jujumoreira.cesinha@gmail.com>
2025-04-06 16:46:29 -0300

    git commit

---
+++ dist/python/src/bancoRedesPetri.py
@@ -0,0 +1,61 @@
+class RedePetriBanco():
+    def __init__(self, saldoInicial = 0.0):
+        self.saldo = saldoInicial
+        self.places = {
+            "Saldo": saldoInicial,
+            "Depósito": 0,
+            "Saque": 0,
+            "Consulta": 0
+        }
+    
+    def depositar(self, valor = 0.0):
+        self.places['Depósito'] += valor
+        self.places['Saldo'] += valor
+        print(f"Depósito de R$ {valor} realizado. Saldo atual: R$ {self.places['Saldo']} \n")
+
+    def sacar(self, valor = 0.0):
+        if self.places['Saldo'] >= valor:
+            self.places['Saque'] += valor
+            self.places['Saldo'] -= valor
+            print(f"Saque de R$ {valor} realizado. Saldo atual: R$ {self.places['Saldo']} \n")
+        else:
+            print("Saldo insuficiente.")
+    
+    def consultar_saldo(self):
+        self.places['Consulta'] += 1
+        print(f"Saldo atual: R$ {self.places['Saldo']} \n")
+    
+    def definir_saldo_inicial(self):
+        valor = float(input("Informe o saldo inicial: "))
+        self.saldo = valor
+        self.places['Saldo'] = valor
+        print(f"Saldo inicial definido como R$ {self.saldo:.2f} \n")
+
+    def menu(self):
+        operando = True
+        while operando:
+            print("Caixa Eletrônico")
+            print("1. Depositar")
+            print("2. Sacar")
+            print("3. Consultar Saldo")
+            print("4. Sair")
+            operacao = int(input("Informe a operação a ser realizada: "))
+            match(operacao):
+                case 1:
+                    valor = float(input("Informe o valor do depósito: "))
+                    self.depositar(valor)
+                case 2:
+                    valor = float(input("Informe o valor do saque: "))
+                    self.sacar(valor)
+                case 3:
+                    self.consultar_saldo()
+                case 4:
+                    print("Saindo...")
+                    operando = False
+                    break
+                case _:
+                    print("Opção inválida. Tente novamente.")
+
+banco = RedePetriBanco()
+banco.definir_saldo_inicial()
+banco.menu()
