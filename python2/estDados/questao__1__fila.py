6059cd72851a8518f4d1f8f74b4deb97ac138f8f
Julio Moreira <jujumoreira.cesinha@gmail.com>
2026-03-02 16:06:25 -0300

    Arquivos dos exercícios da disciplina de Estruturas de Dados.

---
+++ python2/estDados/questao__1__fila.py
@@ -0,0 +1,17 @@
+fila = []
+
+fila.append("José")
+fila.append("Kevin")
+fila.append("Luísa")
+fila.append("Guilherme")
+fila.append("Carolina")
+
+print(f"Pessoas na fila: {fila}")
+
+fila.pop(0)
+
+print(f"Pessoas na fila: {fila}")
+
+fila.pop(0)
+
+print(f"Pessoas na fila: {fila}")
---
+++ python2/estDados/questao__2__fila.py
@@ -0,0 +1,33 @@
+fila = []
+entrada = False
+
+print("Instruções: ")
+print("Digite um nome para adicionar ou: [A] Atender | [V] Verificar | [S] Sair")
+
+while True:    
+    if entrada and len(fila) == 0:
+        print("\nTodos os clientes foram atendidos. Encerrando...")
+        break
+    
+    comando = input("\nComando ou Nome: ").strip().upper()
+
+    if comando.upper() == 'S':
+        print("Encerrando...")
+        break
+
+    elif comando.upper() == 'V':
+        print(f"Fila atual ({len(fila)} pessoas): {fila}")
+    
+    elif comando.upper() == 'A':
+        if not fila:
+            print("Fila vazia.")
+        
+        else:
+            pessoa_atendida = fila.pop(0)
+            print(f"Pessoa atendida: {pessoa_atendida}")
+    
+    else:
+        nome = comando.capitalize()
+        fila.append(nome)
+        entrada = True
+        print(f"{nome} entrou na fila.")
