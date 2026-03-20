31028a9175d8f99c8fcdaec44fa4d697c727f8f2
Julio Moreira <jujumoreira.cesinha@gmail.com>
2026-03-20 19:24:03 -0300

    Código de função fatorial do curso da Cisco.

---
+++ dist/python/cisco_python_course/factorial_function.py
@@ -0,0 +1,14 @@
+def factorial_function(n):
+    if n < 0:
+        return None
+    
+    if n < 2:
+        return 1
+    
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+for i in range(1, 11):
+    print(i, factorial_function(i))
